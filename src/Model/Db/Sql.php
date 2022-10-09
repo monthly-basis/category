3afeca71a4dd7efe5bf27fd38f24ae7fbd0c4229
Leo Galleguillos <leo.galleguillos@icloud.com>
2022-10-09 21:34:28 +0000

    Write CategoryDb\Sql class

---
+++ src/Model/Db/Sql.php
@@ -0,0 +1,7 @@
+<?php
+namespace MonthlyBasis\Category\Model\Db;
+
+class Sql extends \Laminas\Db\Sql\Sql
+{
+
+}
--- src/Module.php
+++ src/Module.php
@@ -1,12 +1,19 @@
 <?php
 namespace MonthlyBasis\Category;
 
+use MonthlyBasis\Category\Model\Db as CategoryDb;
+
 class Module
 {
     public function getServiceConfig()
     {
         return [
             'factories' => [
+                CategoryDb\Sql::class => function ($sm) {
+                    return new CategoryDb\Sql(
+                        $sm->get('category')
+                    );
+                }
             ],
         ];
     }
