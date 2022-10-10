fc92f694df5294f7a9d7550c0a10a705edec0b92
Leo Galleguillos <leo.galleguillos@icloud.com>
2022-10-10 01:16:27 +0000

    Add unique key on `category`.`slug`

--- sql/test/category/create.sql
+++ sql/test/category/create.sql
@@ -4,5 +4,6 @@ CREATE TABLE `category` (
   `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
   `created_datetime` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`category_id`),
+  UNIQUE KEY `slug` (`slug`),
   UNIQUE KEY `name` (`name`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
