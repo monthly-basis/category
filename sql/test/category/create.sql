9c2e41084216554b82d003246fbe8946e470ad03
Leo Galleguillos <leo.galleguillos@icloud.com>
2023-07-19 21:12:05 +0000

    Make `category`.`name` case-insensitive

--- sql/test/category/create.sql
+++ sql/test/category/create.sql
@@ -1,7 +1,7 @@
 CREATE TABLE `category` (
   `category_id` int unsigned NOT NULL AUTO_INCREMENT,
   `slug` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_as_cs NOT NULL,
-  `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_as_cs NOT NULL,
+  `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_as_ci NOT NULL,
   `created_datetime` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`category_id`),
   UNIQUE KEY `slug` (`slug`),
