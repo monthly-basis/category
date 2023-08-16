ae19fd87355f8fbe78150d659b0747c0fe5dfd00
Leo Galleguillos <leo.galleguillos@icloud.com>
2023-08-16 21:52:55 +0000

    Write method to select count(*) where parent_id

--- src/Model/Table/CategoryParentChild.php
+++ src/Model/Table/CategoryParentChild.php
@@ -4,14 +4,32 @@ declare(strict_types=1);
 
 namespace MonthlyBasis\Category\Model\Table;
 
+use Laminas\Db\Adapter\Adapter;
+use Laminas\Db\Adapter\Driver\Pdo\Result;
 use MonthlyBasis\Laminas\Model\Db as LaminasDb;
 
 class CategoryParentChild extends LaminasDb\Table
 {
+    protected Adapter $adapter;
     protected string $table = 'category_parent_child';
 
     public function __construct(
         protected \Laminas\Db\Sql\Sql $sql,
     ) {
+        $this->adapter = $sql->getAdapter();
+    }
+
+    public function selectCountWhereParentId(int $parentId): Result
+    {
+        $sql = '
+            SELECT COUNT(*)
+              FROM `category_parent_child`
+             WHERE `category_parent_child`.`parent_id` = ?
+                 ;
+        ';
+        $parameters = [
+            $parentId,
+        ];
+        return $this->adapter->query($sql)->execute($parameters);
     }
 }
---
+++ test/Model/Table/CategoryParentChildTest.php
@@ -0,0 +1,30 @@
+<?php
+namespace MonthlyBasis\CategoryTest\Model\Table;
+
+use MonthlyBasis\Category\Model\Table as CategoryTable;
+use MonthlyBasis\LaminasTest\TableTestCase;
+
+class CategoryParentChildTest extends TableTestCase
+{
+    protected function setup(): void
+    {
+        $this->setForeignKeyChecks(0);
+        $this->dropAndCreateTable('category_parent_child');
+        $this->setForeignKeyChecks(1);
+
+        $this->categoryParentChildTable = new CategoryTable\CategoryParentChild(
+            $this->getSql()
+        );
+    }
+
+    public function test_selectCountWhereParentId()
+    {
+        $result = $this->categoryParentChildTable->selectCountWhereParentId(
+            1
+        );
+        $this->assertSame(
+            0,
+            $result->current()['COUNT(*)'],
+        );
+    }
+}
